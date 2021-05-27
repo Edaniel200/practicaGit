59293b4f4c01aa1d0f6d56e4af1c23f63f9050f5
Edaniel200 <danielmartinezvazquez8@gmail.com>
2021-05-27 02:53:01 +0200

    Actualizacion desde rama version2

--- Practica Git/js/funcionality.js
+++ Practica Git/js/funcionality.js
@@ -1,5 +1,18 @@
 window.addEventListener("load", () => {
 
-	console.log("Comienzo del proyecto");
+	//console.log("Comienzo del proyecto");
+	let seccion = document.getElementById("seccion");
+	seccion.addEventListener("click", changeColor);
 
 });
+
+
+function changeColor(objeto){
+	console.log("click");
+
+	let ObjectToChange = objeto.target.style;
+
+	ObjectToChange.backgroundColor == "white"?ObjectToChange.backgroundColor = "grey":ObjectToChange.backgroundColor = "white";
+
+
+}
