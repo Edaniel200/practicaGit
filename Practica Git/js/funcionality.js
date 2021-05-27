d446e487b0ab1e66769144f6362554268981d794
Edaniel200 <danielmartinezvazquez8@gmail.com>
2021-05-27 03:14:04 +0200

    Actualizacion desde rama version3

--- Practica Git/js/funcionality.js
+++ Practica Git/js/funcionality.js
@@ -2,17 +2,38 @@ window.addEventListener("load", () => {
 
 	//console.log("Comienzo del proyecto");
 	let seccion = document.getElementById("seccion");
-	seccion.addEventListener("click", changeColor);
+	let texts = document.getElementsByClassName("texts");
+
+	seccion.addEventListener("click", (objeto)=>{ changeColor(objeto, "white", "gey") });
+
+	for(let i = 0; i < texts.length; i++){
+
+		texts[i].addEventListener("click", (objeto)=>{ changeColorText(objeto, "black", "white") });
+	}
+	
 
 });
 
 
-function changeColor(objeto){
-	console.log("click");
 
-	let ObjectToChange = objeto.target.style;
+function changeColor(...args){
 
-	ObjectToChange.backgroundColor == "white"?ObjectToChange.backgroundColor = "grey":ObjectToChange.backgroundColor = "white";
+	//console.log("click");
+
+	let ObjectToChange = args[0].target.style;
+
+	ObjectToChange.backgroundColor == args[1]?ObjectToChange.backgroundColor = args[2]:ObjectToChange.backgroundColor = args[1];
+
+
+}
+function changeColorText(...args){
+
+	//console.log(args[1]);
+
+	let ObjectToChange = args[0].target.style;
+
+	ObjectToChange.color == args[1]?ObjectToChange.color = args[2]:ObjectToChange.color = args[1];
+
 
 
 }
